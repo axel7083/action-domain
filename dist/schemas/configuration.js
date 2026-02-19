a2aeacac24be6257e1dd9d84135dc0528a84d05d
axel7083 <42176370+axel7083@users.noreply.github.com>
2026-02-19 11:55:48 +0100

    refactor: use vite for bundling
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- dist/schemas/configuration.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ConfigurationSchema = exports.OwnerSchema = void 0;
-const zod_1 = require("zod");
-exports.OwnerSchema = zod_1.z.object({ username: zod_1.z.string() });
-exports.ConfigurationSchema = zod_1.z.object({
-    domains: zod_1.z.record(zod_1.z.string(), zod_1.z.array(exports.OwnerSchema))
-});
